727c8c3bd7f47f05dedd71e77c1d941d88638fa0
John Sullivan <jpsullivan2019@gmail.com>
2019-08-11 17:51:00 -0500

    more refactoring

---
+++ assemblers.py
@@ -0,0 +1,6 @@
+# functions to assemble data from various options into strings
+# strings will conform to spec and be able to be passed directly to the qr generator
+
+def assemble_wifi(data):
+
+    return st
